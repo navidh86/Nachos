6d286f7ad5c58d6ba70d27e6d04fdbb29347f686
navidh86 <navidhasan0@gmail.com>
2020-11-09 22:00:14 +0600

    Update AlarmTest (use 3 separate threads to test)

--- proj1/AlarmTest.java
+++ proj1/AlarmTest.java
@@ -1,6 +1,7 @@
 package nachos.proj1;
 
 import nachos.machine.Machine;
+import nachos.threads.KThread;
 import nachos.threads.ThreadedKernel;
 
 /**
@@ -9,8 +10,32 @@ import nachos.threads.ThreadedKernel;
  */
 public class AlarmTest {
     public static void startTest() {
-        System.out.println("Current time: " + Machine.timer().getTime());
-        ThreadedKernel.alarm.waitUntil(1000);
-        System.out.println("Current time: " + Machine.timer().getTime());
+        System.out.println("Start of alram test . . .");
+        
+        KThread at1 = new KThread(new AlarmRunnable(3000)).setName("Alarm Thread 1");
+        KThread at2 = new KThread(new AlarmRunnable(2000)).setName("Alarm Thread 2");
+        KThread at3 = new KThread(new AlarmRunnable(1000)).setName("Alarm Thread 3");
+        
+        at1.fork();
+        at2.fork();
+        at3.fork();
+        
+        at1.join();
+        at2.join();
+        at3.join();
     }
+}
+
+class AlarmRunnable implements Runnable {
+    AlarmRunnable(long waitTime) {
+        this.waitTime = waitTime;
+    }
+    
+    public void run() {
+        System.out.println(KThread.currentThread().getName() + ": Current time: " + Machine.timer().getTime());
+        ThreadedKernel.alarm.waitUntil(waitTime);
+        System.out.println(KThread.currentThread().getName() + ": Current time: " + Machine.timer().getTime());
+    }
+    
+    private long waitTime;
 }
