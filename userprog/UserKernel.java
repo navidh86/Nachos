f4a82d3b0d0bc3726dc506c494114458c5d5e243
navidh86 <navidhasan0@gmail.com>
2020-11-11 21:35:58 +0600

    Maintain a linked list of free physical pages, allocate a new page on request atomically

--- userprog/UserKernel.java
+++ userprog/UserKernel.java
@@ -1,5 +1,8 @@
 package nachos.userprog;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
 import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
@@ -27,6 +30,16 @@ public class UserKernel extends ThreadedKernel {
 	Machine.processor().setExceptionHandler(new Runnable() {
 		public void run() { exceptionHandler(); }
 	    });
+        
+        //initialize the list of free pages
+        freePages = new LinkedList<>();
+        for (int i=0; i<Machine.processor().getNumPhysPages(); i++)
+            freePages.add(i);
+        
+        //for testing, shuffling the list
+        Collections.shuffle(freePages, new Random());
+        
+        pageLock = new Lock();
     }
 
     /**
@@ -107,9 +120,42 @@ public class UserKernel extends ThreadedKernel {
 	super.terminate();
     }
 
+    /**
+     * Return a free physical page number from the free list and remove it from the list
+     */
+    public static int getNewPage() {
+        pageLock.acquire();
+        
+        int ppn = -1;
+        
+        if (!freePages.isEmpty()) {
+            ppn = freePages.removeFirst();
+        }
+        
+        pageLock.release();
+        
+        return ppn;
+    }
+    
+    /**
+     * Add back the particular page to the free list
+     */
+    public static void freePage(int ppn) {
+        pageLock.acquire();
+        
+        freePages.add(ppn);
+        
+        pageLock.release();
+    }
+    
+    
     /** Globally accessible reference to the synchronized console. */
     public static SynchConsole console;
 
     // dummy variables to make javac smarter
     private static Coff dummy1 = null;
+    
+    /** Global linked list of free physical pages */
+    private static LinkedList<Integer> freePages;
+    private static Lock pageLock;
 }
