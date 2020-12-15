94bf192f19eeca3ffa3751c0947f5ea6c247d9e0
navidh86 <navidhasan0@gmail.com>
2020-12-15 19:37:12 +0600

    Implement the Pair class

--- vm/Pair.java
+++ vm/Pair.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nachos.vm;
 
-/**
- *
- * @author navidh86
- */
+import java.util.Objects;
+
 public class Pair {
+    public Pair(Integer first, Integer second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        final Pair other = (Pair) obj;
+        
+        return Objects.equals(this.first, other.first) && Objects.equals(this.second, other.second);
+    }
     
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.first);
+        hash = 97 * hash + Objects.hashCode(this.second);
+        
+        return hash;
+    }
+    
+    public final Integer first, second;
 }
